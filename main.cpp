f0c9d3c467b3f7df387d4920728962d37b854ac5
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 13:03:11 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+By default access to members of a C++ class is private. 
+
+The private members are not accessible outside the class; 
+they can be accessed only through methods of the class. 
+
+The public members form an interface to the class and 
+are accessible outside the class.
